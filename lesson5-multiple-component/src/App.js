5faf8ba0ceef147f8f6d64c6302a9a19765c713f
Habibullo2003 <xayrullayevhabibullo745@gmail.com>
2025-08-05 20:37:56 +0500

    use from React Fragment

--- lesson5-multiple-component/src/App.js
+++ lesson5-multiple-component/src/App.js
@@ -1,26 +1,70 @@
-import './App.css';
-import Title from './components/Title';
+import './App.css'
+import { useState } from 'react'
+import Title from './components/Title'
+import { Fragment } from 'react'
 
 function App() {
+  const [showContent, setShowContent] = useState(true)
+  const [events, setEvents] = useState([
+    { title: "Habibullo's birthday party", id: 1 },
+    { title: "Doston's live stream", id: 2 },
+    { title: 'match: Realmadrid vs Barselona', id: 3 },
+  ])
+
+
+  const handleDelete = (id) => {
+    // const filteredEvents = events.filter((event) => {
+    //   return event.id !== id
+    // })
+    // setEvents(filteredEvents)
+
+    // setEvents(events.filter((event) => {
+    //   return event.id !== id
+    // }))
+
+    setEvents((prev) => {
+      return prev.filter((event) => {
+        return event.id !== id
+      })
+    })
+  }
+
+  let subtitle = 'All events well be here :)'
+
   return (
     <div className="App">
-      <Title title="Events" subtitle="All events well be here :)"/>
-      <header className="App-header">
-        <img src="./logo.svg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title title="Events" subtitle={subtitle} />
+      {showContent && (
+        <button
+          onClick={() => {
+            setShowContent(false)
+          }}>
+          Hide Content
+        </button>
+      )}
+      {!showContent && (
+        <button
+          onClick={() => {
+            setShowContent(true)
+          }}>
+          Show Content
+        </button>
+      )}
+      {showContent && (
+        <div>
+          {events.length === 0 && <h3>No Content Yet :(</h3>}
+          {events.map((event) => {
+            return (
+              <Fragment key={event.id}>
+                <h2>{event.title}</h2>
+                <button onClick={() => handleDelete(event.id)}>Delete</button>
+              </Fragment>
+            )
+          })}
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
