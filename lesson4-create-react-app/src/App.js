9a38e73805440ace6d2405738c9987bc4f53d23c
Habibullo2003 <xayrullayevhabibullo745@gmail.com>
2025-08-05 19:49:46 +0500

    hide && show content in useState

--- lesson4-create-react-app/src/App.js
+++ lesson4-create-react-app/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import { useState } from 'react';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
-
   const [name, setName] = useState('Habibullo')
+  const [showContent, setShowContent] = useState(true)
   const [events, setEvents] = useState([
-    {title: "Habibullo's birthday party", id: 1},
-    {title: "Doston's live stream", id: 2},
-    {title: "match: Realmadrid vs Barselona", id:3}
+    { title: "Habibullo's birthday party", id: 1 },
+    { title: "Doston's live stream", id: 2 },
+    { title: 'match: Realmadrid vs Barselona', id: 3 },
   ])
 
   const handleClick = () => {
@@ -30,21 +30,44 @@ function App() {
       })
     })
   }
-  
+
   return (
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={handleClick}>Change name</button>
-      {events.map((event) => {
-        return (
-          <div key={event.id}>
-            <h2>{event.title}</h2>
-            <button onClick={() => handleDelete(event.id)}>Delete</button>
-          </div>
-        )
-      })}
+      <hr />
+      <br />
+      {showContent && (
+        <button
+          onClick={() => {
+            setShowContent(false)
+          }}>
+          Hide Content
+        </button>
+      )}
+      {!showContent && (
+        <button
+          onClick={() => {
+            setShowContent(true)
+          }}>
+          Show Content
+        </button>
+      )}
+      {showContent && (
+        <div>
+          {events.length === 0 && <h3>No Content Yet :(</h3>}
+          {events.map((event) => {
+            return (
+              <div key={event.id}>
+                <h2>{event.title}</h2>
+                <button onClick={() => handleDelete(event.id)}>Delete</button>
+              </div>
+            )
+          })}
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
