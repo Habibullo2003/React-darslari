ca58c50f64a53ee9776aa5e98d9db7702cfe7087
Habibullo2003 <xayrullayevhabibullo745@gmail.com>
2025-08-05 19:18:01 +0500

    use object in useState

--- lesson4-create-react-app/src/App.js
+++ lesson4-create-react-app/src/App.js
@@ -4,6 +4,11 @@ import { useState } from 'react';
 function App() {
 
   const [name, setName] = useState('Habibullo')
+  const [events, setEvents] = useState([
+    {title: "Habibullo's birthday party", id: 1},
+    {title: "Doston's live stream", id: 2},
+    {title: "match: Realmadrid vs Barselona", id:3}
+  ])
 
   const handleClick = () => {
     setName('Doston')
@@ -13,6 +18,13 @@ function App() {
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={handleClick}>Change name</button>
+      {events.map((event) => {
+        return (
+          <div key={event.id}>
+            <h2>{event.title}</h2>
+          </div>
+        )
+      })}
     </div>
   );
 }
