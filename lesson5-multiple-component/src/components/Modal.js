eb04f79f73c30f8727d2e6814b227d557e15c416
Habibullo2003 <xayrullayevhabibullo745@gmail.com>
2025-08-07 05:27:19 +0500

    Inline Css

--- lesson5-multiple-component/src/components/Modal.js
+++ lesson5-multiple-component/src/components/Modal.js
@@ -1,16 +1,23 @@
-import "./Modal.css"
+import './Modal.css'
 import ReactDOM from 'react-dom'
 
-function Modal({children, closeModal}) {
-  return ReactDOM.createPortal((
+function Modal({ children, closeModal }) {
+  return ReactDOM.createPortal(
     <div className="modal-backdrop">
-      <div className="modal">
+      <div
+        className="modal"
+        style={{
+          border: '4px solid',
+          borderColor: 'red',
+          textAlign: 'center',
+        }}>
         {children}
         <br />
         <button onClick={closeModal}>Close</button>
       </div>
-    </div>
-  ), document.body)
+    </div>,
+    document.body
+  )
 }
 
 export default Modal
