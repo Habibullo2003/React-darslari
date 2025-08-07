59cac9b6a49d280c46cbe0d7d67736eba543d5a5
Habibullo2003 <xayrullayevhabibullo745@gmail.com>
2025-08-07 05:21:53 +0500

    Global va Component Styles

--- lesson5-multiple-component/src/components/Title.js
+++ lesson5-multiple-component/src/components/Title.js
@@ -1,10 +1,10 @@
 const Title = ({title, subtitle}) => {
   return (
-      <>
+      <div className="title-block">
           <h1 className="title">{title}</h1>
           <br />
           <p className="subtitle">{subtitle}</p>
-      </>
+      </div>
   )
 }
 
