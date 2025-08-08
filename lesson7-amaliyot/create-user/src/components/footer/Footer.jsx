ef173adb9c0759a47d37c452ee9469e6c85fe677
Habibullo2003 <xayrullayevhabibullo745@gmail.com>
2025-08-08 22:21:44 +0500

    Create User(CUser) amaliyoti

---
+++ lesson7-amaliyot/create-user/src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+// style
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container container">
+        <p>All right reserved. <a href="https://github.com/Habibullo2003">Habibullo Xayrullayev</a></p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
