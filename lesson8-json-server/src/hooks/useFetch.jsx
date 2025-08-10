2f323e24172d6d9cce26e9b7d3228fce665b259f
Habibullo2003 <xayrullayevhabibullo745@gmail.com>
2025-08-10 16:20:24 +0500

    Error bilan ishlash

--- lesson8-json-server/src/hooks/useFetch.jsx
+++ lesson8-json-server/src/hooks/useFetch.jsx
@@ -1,19 +1,29 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
 export function useFetch(url) {
   const [data, setData] = useState(null)
   const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
       setIsPending(true)
-      const req = await fetch(url)
-      const data = await req.json()
-      setData(data)
-      setIsPending(false)
+      try {
+        const req = await fetch(url)
+        if (!req.ok) {
+          throw new Error(req.statusText)
+        }
+        const data = await req.json()
+        setData(data)
+        setIsPending(false)
+      } catch (err) {
+        console.log(err.message)
+        setError(err.message)
+        setIsPending(false)
+      }
     }
     fetchData()
   }, [url])
 
-  return { data, isPending }
+  return { data, isPending, error }
 }
