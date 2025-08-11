c4e0e6984b19df3c94f102e47e3d3190c109fa25
Habibullo2003 <xayrullayevhabibullo745@gmail.com>
2025-08-11 18:58:45 +0500

    Codlarni Yangi verisayaga almashitirsh

--- lesson9-react-router/src/App.jsx
+++ lesson9-react-router/src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
+import RootLayout from './layout/RootLayout'
 
 // react=router-dom
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom'
 
 // pages
 import Home from './pages/Home'
@@ -9,22 +15,18 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 
 function App() {
+  const routes = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<RootLayout/>}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    )
+  )
   return (
     <div className="App">
-      <BrowserRouter>
-        <nav>
-          <h1>My Articles</h1>
-           <NavLink to="/">Home</NavLink>          
-           <NavLink to="/about">About</NavLink>
-           <NavLink to="/contact">Contact</NavLink>
-        </nav>
-
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/about' element={<About />}/>
-          <Route path='/contact' element={<Contact />}/>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={routes} />
     </div>
   )
 }
