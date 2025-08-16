11d0f2129ed42c23347dde4a5b737295a2135af7
Habibullo2003 <xayrullayevhabibullo745@gmail.com>
2025-08-16 19:29:31 +0500

    ArticlesLayout

--- lesson9-react-router/src/App.jsx
+++ lesson9-react-router/src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
+
+// layout
 import RootLayout from './layout/RootLayout'
 import ContactLayout from './layout/ContactLayout'
+import ArticlesLayout from './layout/ArticlesLayout'
+
 
 // react=router-dom
 import {
@@ -16,6 +20,7 @@ import About from './pages/About'
 import Faq from './pages/help/Faq'
 import Form from './pages/help/Form'
 import PageNotFound from './pages/PageNotFound'
+import Articles from './pages/articles/Articles'
 
 function App() {
   const routes = createBrowserRouter(
@@ -26,6 +31,9 @@ function App() {
         <Route path="/contact" element={<ContactLayout />} >
             <Route path='faq' element={<Faq/>}/>
             <Route path='form' element={<Form/>}/>
+        </Route>
+        <Route path='articles' element={<ArticlesLayout/>}>
+          <Route index element={<Articles/>}/>
         </Route>
         {/* PageNotFound */}
         <Route path='*' element={<PageNotFound/>}/>
